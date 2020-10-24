3d1b35f197e320765fbcfae7832870dc8d36cd56
tombanach <tom.banach98@gmail.com>
2020-10-25 00:13:27 +0200

    Add project files.

---
+++ Wsei.ExchangeThings.Web/Controllers/ExchangesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wsei.ExchangeThings.Web.Filters;
+using Wsei.ExchangeThings.Web.Models;
+
+namespace Wsei.ExchangeThings.Web.Controllers
+{
+    public class ExchangesController : Controller
+    {
+        [ServiceFilter(typeof(MyCustomActionFilter))]
+        public IActionResult Show(string id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ItemModel item)
+        {
+            // TODO add to database
+
+            var viewModel = new AddNewItemConfirmationViewModel
+            {
+                Id = 1,
+                Name = item.Name,
+            };
+
+            return View("AddConfirmation", viewModel);
+        }
+    }
+}
